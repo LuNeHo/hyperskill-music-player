5baf46810f77cd06d390f547fe02b9405a87c4b5
LuNeHo <tomelu@pm.me>
2026-06-26 14:50:05 -0400

    Prepare UI layout structure

--- Music Player/task/src/main/java/org/hyperskill/musicplayer/MainActivity.kt
+++ Music Player/task/src/main/java/org/hyperskill/musicplayer/MainActivity.kt
@@ -1,11 +1,58 @@
 package org.hyperskill.musicplayer
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import org.hyperskill.musicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initActions()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.mainMenuAddPlaylist -> {
+            Toast.makeText(this, R.string.search_error_no_songs_loaded, Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        R.id.mainMenuLoadPlaylist -> {
+            getLoadPlaylistDialog().show()
+            true
+        }
+
+        R.id.mainMenuDeletePlaylist -> {
+            getDeletePlaylistDialog().show()
+            true
+        }
+
+        else -> super.onOptionsItemSelected(item)
+    }
+
+    private fun initActions() {
+        binding.mainButtonSearch.setOnClickListener {
+            Toast.makeText(this, R.string.search_error_no_songs_found, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getLoadPlaylistDialog() = AlertDialog.Builder(this)
+        .setTitle(R.string.dialog_load_playlist_title)
+        .setNegativeButton(android.R.string.cancel, null)
+
+    private fun getDeletePlaylistDialog() = AlertDialog.Builder(this)
+        .setTitle(R.string.dialog_delete_playlist_title)
+        .setNegativeButton(android.R.string.cancel, null)
 }
---
+++ Music Player/task/src/main/java/org/hyperskill/musicplayer/MainPlayerControllerFragment.kt
@@ -0,0 +1,35 @@
+package org.hyperskill.musicplayer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.hyperskill.musicplayer.databinding.FragmentMainPlayerControllerBinding
+
+class MainPlayerControllerFragment : Fragment() {
+    private lateinit var binding: FragmentMainPlayerControllerBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainPlayerControllerBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        with(binding) {
+            controllerTvCurrentTime.text = getText(R.string.player_controller_init_time)
+            controllerTvTotalTime.text = getText(R.string.player_controller_init_time)
+            controllerBtnPlayPause.text = getText(R.string.controller_btn_play_pause)
+            controllerBtnStop.text = getText(R.string.controller_btn_stop)
+        }
+    }
+}
